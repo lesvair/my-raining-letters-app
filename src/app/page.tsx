d5a61aee1e27fcfb120a86fbbbd27c4b181fd595
Les Sander <lessander@Less-MacBook-Pro.local>
2025-06-28 14:57:24 -0500

    mobile optimization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,8 +44,8 @@ class TextScramble {
     for (let i = 0; i < length; i++) {
       const from = oldText[i] || ''
       const to = newText[i] || ''
-      const start = Math.floor(Math.random() * 20)
-      const end = start + Math.floor(Math.random() * 20)
+      const start = Math.floor(Math.random() * 30)
+      const end = start + Math.floor(Math.random() * 30)
       this.queue.push({ from, to, start, end })
     }
     
