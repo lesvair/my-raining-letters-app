3dae493dade9a5233bf98a2418808a3f7587d836
Les Sander <lessander@Less-MacBook-Pro.local>
2025-07-07 20:40:32 -0500

    add the c

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Seuroso",
+  title: "Securoso",
   description: "Seuroso - Keeping you Safe Today and Tomorrow",
 };
 
